74764a4900f207c3baf74a713035267914d42f11
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2019-10-03 21:03:06 +0100

    entry msgs now clearer, began using string directions, more comments

--- battleship.py
+++ battleship.py
@@ -10,14 +10,14 @@ def greet():
     time.sleep(1)
     print("HELLO "+name+".")
 
-def orientationSeparator(orientation): #converts 4 integer directions into coordinate steps
-    if orientation == 1: #right
+def orientationSeparator(orientation): #converts string directions into coordinate steps
+    if orientation == 'right': #right
         modx = 1
         mody = 0
-    elif orientation == 2: #up
+    elif orientation == 'up': #up
         modx = 0
         mody = -1
-    elif orientation == 3: #left
+    elif orientation == 'left': #left
         modx = -1
         mody = 0
     else: #(4) down
@@ -44,26 +44,28 @@ class Board: #the board for ships and the board for guesses
         return board
 
     def placeShip(self,ship,orientation,x,y): #modifies the board to have a ship on it
-        savedLayout = self.layout
-        #ship is an integer from 1-5
-        #orientation is an integer from 1-4
+        savedLayout = self.layout #saves layout for possible overlaps
+        #ship is in range(6)
+        #orientation is a direction string
         #x is the x position of the head of the ship
         #y is the y position of the head of the ship
         #self.layout is expected to be a 10x10 2D array
         modx,mody = orientationSeparator(orientation)
         #print(x,y)
         for counter in range(0,ship):
-            if self.layout[y][x] == 0:
-                self.layout[y][x] = ship
+            if self.layout[y][x] == 0: #tests that placement space is empty
+                self.layout[y][x] = ship #places ship
                 x += modx
                 y += mody
             else:
                 print('SHIP OVERLAP: PLACE ELSEWHERE')
                 self.layout = savedLayout
                 print('place your ship of length',ship)
-                newX = int(input('enter the column you wish to place the ship'))-1
-                newY = int(input('enter the row you wish to place the ship'))-1
-                newOrientation = int(input('enter the ship orientation.'))
+                newX = int(input('enter the column you wish to place the ship head: '))-1
+                newY = int(input('enter the row you wish to place the ship head: '))-1
+                newOrientation = input('enter the ship orientation: ')
+                while newOrientation not in ['left','right','up','down']:
+                    newOrientation = input('enter right/left/up/down: ')
                 self.placeShip(ship,newOrientation,newX,newY)
             #print(x,y)
             #showBoard(board)
@@ -77,9 +79,11 @@ def setup():
     playerBoard.showBoard()
     for counter in range(1,6):
         print('place your ship of length',counter)
-        x = int(input('enter the column you wish to place the ship'))-1
-        y = int(input('enter the row you wish to place the ship'))-1
-        orientation = int(input('enter the ship orientation.'))
+        x = int(input('enter the column you wish to place the ship head: '))-1
+        y = int(input('enter the row you wish to place the ship head: '))-1
+        orientation = input('enter the ship orientation: ')
+        while orientation not in ['left','right','up','down']:
+            orientation = input('enter right/left/up/down: ')
         playerBoard.placeShip(counter,orientation,x,y)
 
         playerBoard.showBoard()
@@ -95,6 +99,10 @@ def coordinateParser(coordString): #turns A3 into (0,2)
 def guess():
     guess = input('enter bombing coordinates: ')
     guessCol,guessRow = coordinateParser(guess)
+    #if enemygueses has a ship
+    #add x print hit
+    #else
+    #add sometihng print miss
     return
 
 def main():
