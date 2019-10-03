24df8fbb28ffaab9babeb99da5224e9ce3bf1883
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2019-10-03 20:46:19 +0100

    added comments and checks for ship overlap

--- battleship.py
+++ battleship.py
@@ -1,5 +1,5 @@
 #battleship
-import time
+import time #used for delaying output nicely
 
 def greet():
     print("WOULD YOU LIKE TO PLAY A GAME?")
@@ -10,40 +10,41 @@ def greet():
     time.sleep(1)
     print("HELLO "+name+".")
 
-def orientationSeparator(orientation):
-    if orientation == 1:
+def orientationSeparator(orientation): #converts 4 integer directions into coordinate steps
+    if orientation == 1: #right
         modx = 1
         mody = 0
-    elif orientation == 2:
+    elif orientation == 2: #down
         modx = 0
         mody = -1
-    elif orientation == 3:
+    elif orientation == 3: #left
         modx = -1
         mody = 0
-    else:
+    else: #(4) up
         modx = 0
         mody = 1
     return modx, mody
 
-class Board:
-    def __init__(self,height,width):
+class Board: #the board for ships and the board for guesses
+    def __init__(self,height,width): #instantiates a board
         self.height = height
         self.width = width
         self.layout = self.generateEmptyBoard()
 
-    def showBoard(self):
+    def showBoard(self): #displays the board nicely
         for y in range(self.height):
             for x in range(self.width):
                 print(self.layout[y][x],' ',end = '')
             print()
 
-    def generateEmptyBoard(self):
+    def generateEmptyBoard(self): #returns an empty board of the object's dimensions
         board = []
         for counter in range(self.height):
             board.append([0]*self.width)
         return board
 
-    def placeShip(self,ship,orientation,x,y):
+    def placeShip(self,ship,orientation,x,y): #modifies the board to have a ship on it
+        savedLayout = self.layout
         #ship is an integer from 1-5
         #orientation is an integer from 1-4
         #x is the x position of the head of the ship
@@ -52,9 +53,18 @@ class Board:
         modx,mody = orientationSeparator(orientation)
         #print(x,y)
         for counter in range(0,ship):
-            self.layout[y][x] = ship
-            x += modx
-            y += mody
+            if self.layout[y][x] == 0:
+                self.layout[y][x] = ship
+                x += modx
+                y += mody
+            else:
+                print('SHIP OVERLAP: PLACE ELSEWHERE')
+                self.layout = savedLayout
+                print('place your ship of length',ship)
+                newX = int(input('enter the column you wish to place the ship'))-1
+                newY = int(input('enter the row you wish to place the ship'))-1
+                newOrientation = int(input('enter the ship orientation.'))
+                self.placeShip(ship,newOrientation,newX,newY)
             #print(x,y)
             #showBoard(board)
 
@@ -76,10 +86,10 @@ def setup():
 
     return enemyBoard,enemyGuesses,playerBoard,playerGuesses
 
-def coordinateParser(coordString):
-    xy = coordString.split()
-    x = ord(xy[0].lower()) - 97
-    y = xy[1]
+def coordinateParser(coordString): #turns A3 into (0,2)
+    xy = coordString.split() #separates A3 into ['A','3']
+    x = ord(xy[0].lower()) - 97 #turns A > a > 97 > 0, B > b > 98 > 1
+    y = xy[1]-1
     return x,y
 
 def guess():
@@ -90,6 +100,5 @@ def guess():
 def main():
     greet()
     enemyBoard,enemyGuesses,playerBoard,playerGuesses = setup()
-
 
 main()
