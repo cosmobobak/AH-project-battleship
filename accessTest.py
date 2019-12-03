3374664f5f2d7ba815de174d91ca940e83154f9e
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2019-12-03 19:35:55 +0000

    started using test path & table name

--- accessTest.py
+++ accessTest.py
@@ -1,8 +1,8 @@
 import pyodbc
 
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=path where you stored the Access file\file name.accdb;')
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Cosmo\Documents\test1.accdb;')
 cursor = conn.cursor()
-cursor.execute('select * from table name')
+cursor.execute('select * from testTable1')
 
 for row in cursor.fetchall():
     print (row)
