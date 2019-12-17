68e49487bf5f3f402c0b267ee76471f2b32fa6a8
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2019-12-17 20:45:45 +0000

    Update accessTest.py

--- accessTest.py
+++ accessTest.py
@@ -2,7 +2,19 @@ import pyodbc
 
 conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Cosmo\Documents\test1.accdb;')
 cursor = conn.cursor()
-cursor.execute('select * from testTable1')
+cursor.execute(
+'''
+INSERT INTO testTable1(ID,Word,Iteration)
+VALUES(3,'test',19);
+'''
+)
+
+cursor.execute(
+'''
+SELECT * FROM testTable1;
+'''
+)
+
 
 for row in cursor.fetchall():
-    print (row)
+    print(row)
