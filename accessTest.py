b6c679dd730cd4657d6564fbfdbc7a7be1915235
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2019-12-19 13:57:58 +0000

    altered accessTest

--- accessTest.py
+++ accessTest.py
@@ -1,8 +1,20 @@
 import pyodbc
 
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Cosmo\Documents\test1.accdb;')
+driver = 'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
+path = r'DBQ=C:\Users\bobakcjs\Documents\GitHub\battleship\\'
+database = 'battleshipDB'
+connstring = driver + path + database + '.accdb;'
+conn = pyodbc.connect(connstring, autocommit=True)
 cursor = conn.cursor()
-cursor.execute('select * from testTable1')
+
+cursor.execute('select * from Table1')
 
 for row in cursor.fetchall():
-    print (row)
+    print(row)
+
+cursor.execute("insert into Table1 values(3,'data7','data8','data9')")
+
+cursor.execute('select * from Table1')
+
+for row in cursor.fetchall():
+    print(row)
